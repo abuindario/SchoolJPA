eea9976574dbd3983b9fa371f7267647ad80d750
abuindario <darioabuinpose@gmail.com>
2023-09-20 13:23:25 +0200

    Fixed bug: student could be added twice to the same subject

--- src/main/java/ejEscuelaJPA/BO/Student_SubjectBO.java
+++ src/main/java/ejEscuelaJPA/BO/Student_SubjectBO.java
@@ -1,5 +1,8 @@
 package ejEscuelaJPA.BO;
 
+import java.util.Iterator;
+import java.util.List;
+
 import javax.persistence.EntityManager;
 
 import ejEscuelaJPA.DAO.Student_SubjectDAO;
@@ -18,7 +21,23 @@ public class Student_SubjectBO {
 		student_subject.setStudent(student);
 		student_subject.setSubject(subject);
 		Student_SubjectDAO student_subjectDAO = new Student_SubjectDAO();
+		
+		//Prevent that one student can be added twice to the same subject
+		List<Student_Subject> ss = student_subjectDAO.getStudentSubject(em);
+		boolean isIn = false;
+		Iterator<Student_Subject> it = ss.iterator();
+		while(it.hasNext()) {
+			Student_Subject stsu = it.next();
+			if(stsu.getStudent().equals(student_subject.getStudent()) && stsu.getSubject().equals(student_subject.getSubject())) {
+				isIn = true;
+				System.out.println(stsu.getStudent().getName() + " is already in subject " + stsu.getSubject().getName());
+			}
+		}
+		if(!isIn) {
 		student_subjectDAO.insert(em, student_subject);
+		}
+		
+		
 		em.getTransaction().commit();
 		em.close();
 	}
--- src/main/java/ejEscuelaJPA/DAO/Student_SubjectDAO.java
+++ src/main/java/ejEscuelaJPA/DAO/Student_SubjectDAO.java
@@ -1,11 +1,19 @@
 package ejEscuelaJPA.DAO;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
 
 import ejEscuelaJPA.entities.Student_Subject;
 
 public class Student_SubjectDAO {
 	public void insert(EntityManager em, Student_Subject ss) {
 		em.persist(ss);
+	}
+	
+	public List<Student_Subject> getStudentSubject(EntityManager em) {
+		TypedQuery<Student_Subject> query = em.createQuery("FROM Student_Subject ORDER BY id", Student_Subject.class);
+		return query.getResultList();
 	}
 }
